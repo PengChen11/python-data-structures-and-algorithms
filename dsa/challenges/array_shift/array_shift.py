66bc009e222aa411f470526d9e68a71d488d901c
Peng Chen <pchen0816@outlook.com>
2020-05-12 12:39:45 -0700

    challenge 2 done

---
+++ dsa/challenges/array_shift/array_shift.py
@@ -0,0 +1,41 @@
+# if I can't use any build-in array method
+def insertShiftArray(inputArray, num):
+  newArray=[0]*(len(inputArray)+1)
+  for i in range(len(inputArray)):
+    if i <(len(inputArray)+1)//2:
+      newArray[i]=inputArray[i]
+    elif i == (len(inputArray)+1)//2:
+      newArray[i]=num
+      newArray[i+1]=inputArray[i]
+    else:
+      newArray[i+1]=inputArray[i]
+  return newArray
+
+
+# if I can use list.append method
+def insertShiftArray2(inputArray, num):
+  newArray=[]
+  for i in range(len(inputArray)):
+    if i==(len(inputArray)+1)//2:
+      newArray.append(num)
+      newArray.append(inputArray[i])
+    else:
+      newArray.append(inputArray[i])
+  return newArray
+
+
+#Here's the stretch goal to remove the element with middle index from input array
+# it is not using any build-in list method
+def removeShiftArray(inputArray):
+  newArray = [0]*(len(inputArray)-1)
+  for i in range(len(inputArray)):
+    if i <(len(inputArray))//2:
+      newArray[i]=inputArray[i]
+    elif i == (len(inputArray))//2:
+      continue
+    else:
+      newArray[i-1]=inputArray[i]
+  return newArray
+
+
+
