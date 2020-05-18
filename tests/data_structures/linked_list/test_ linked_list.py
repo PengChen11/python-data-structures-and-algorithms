04c1213bf5f0dfa7d033b111a675f97512fffcc4
Peng Chen <pchen0816@outlook.com>
2020-05-18 09:06:58 -0700

    tester modified

--- tests/data_structures/linked_list/test_ linked_list.py
+++ tests/data_structures/linked_list/test_ linked_list.py
@@ -5,28 +5,28 @@ import pytest
 
 def test_instantiate():
     tester = Linked_List()
-    assert tester.head_val==None
+    assert(tester.head_val==None), 'Error occured when instantiate a linked list'
 
 
 def test_insert_single(init_values):
     test_list = Linked_List()
     test_list.insert(['Day1: Mon'])
     tester=test_list.__str__()
-    assert tester == '{ Day1: Mon } -> NULL'
+    assert (tester == '{ Day1: Mon } -> NULL'), 'Failed to insert a single value to linked list'
 
 
 def test_insert_array():
     test_list = Linked_List()
     test_list.insert([[1,2,3],[4,5,6]])
     tester=test_list.__str__()
-    assert tester == '{ [1, 2, 3] } -> { [4, 5, 6] } -> NULL'
+    assert (tester == '{ [1, 2, 3] } -> { [4, 5, 6] } -> NULL'), 'Failed to insert a list into the linked list'
 
 
 def test_head(init_values):
     test_list = Linked_List()
     test_list.insert(init_values)
     tester = test_list.head_val.val
-    assert tester == 'Day2: Tue'
+    assert (tester == 'Day2: Tue'),'HEAD test failed'
 
 
 def test_insert_multiple(init_values, muti_values):
@@ -34,7 +34,7 @@ def test_insert_multiple(init_values, muti_values):
     test_list.insert(init_values)
     test_list.insert(muti_values)
     tester=test_list.__str__()
-    assert tester == '{ Day7: Sun } -> { Day1: Mon } -> { Day2: Tue } -> { Day3: Wed } -> { Day4: Thu } -> { Day5: Fri } -> NULL'
+    assert (tester == '{ Day7: Sun } -> { Day1: Mon } -> { Day2: Tue } -> { Day3: Wed } -> { Day4: Thu } -> { Day5: Fri } -> NULL'), 'Failed to insert multiple values into linked list'
 
 
 def test_search_value_true(init_values):
@@ -42,15 +42,14 @@ def test_search_value_true(init_values):
     test_list.insert(init_values)
     test_1 = test_list.includes('Day2: Tue')
     test_2 = test_list.includes('Day5: Fri')
-    assert test_1 == True
-    assert test_2 == True
+    assert (test_1 == True and test_2 == True), 'Failed to search inside the linked list'
 
 
 def test_search_array():
     test_list = Linked_List()
     test_list.insert([[1,2,3],[4,5,6]])
     tester=test_list.includes([4,5,6])
-    assert tester == True
+    assert (tester == True),'Failed to search an array inside the linked list'
 
 
 def test_search_value_false(init_values):
@@ -58,15 +57,14 @@ def test_search_value_false(init_values):
     test_list.insert(init_values)
     test_1 = test_list.includes('Day1: Mon')
     test_2 = test_list.includes('Day5: Tue')
-    assert test_1 == False
-    assert test_2 == False
+    assert (test_1 == False and test_2 == False),'Failed to search something not in the linked list'
 
 
 def test_return_all(init_values):
     test_list = Linked_List()
     test_list.insert(init_values)
     tester = test_list.__str__()
-    assert tester == '{ Day2: Tue } -> { Day3: Wed } -> { Day4: Thu } -> { Day5: Fri } -> NULL'
+    assert (tester == '{ Day2: Tue } -> { Day3: Wed } -> { Day4: Thu } -> { Day5: Fri } -> NULL'),'Failed to return all values inside of the linked list'
 
 
 
