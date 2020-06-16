fe248c8ccb8b7aa1b0fb9bcc04dbcbb466e4e4ca
Peng Chen <pchen0816@outlook.com>
2020-06-16 09:40:35 -0700

    another way

--- dsa/challenges/insertion_sort/insertion_sort.py
+++ dsa/challenges/insertion_sort/insertion_sort.py
@@ -2,17 +2,17 @@
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         j = i - 1
-        temp = arr[i]
+        # temp = arr[i]
 
-        while j>= 0 and temp < arr[j]:
-            arr[j+1] = arr[j]
+        while j>= 0 and arr[j+1] < arr[j]:
+            arr[j+1], arr[j] = arr[j],arr[j+1]
             j = j-1
 
-        arr[j+1] = temp
+        # arr[j+1] = temp
     return arr
 
 
 if __name__ == "__main__":
-    arr = [20,18,12,8,5,-2]
+    arr = [8,4,23,42,16,15]
     test = insertion_sort(arr)
     print(test)
